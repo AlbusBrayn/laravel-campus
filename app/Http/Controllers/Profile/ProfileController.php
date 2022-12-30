80abb6f1325bc4899925ceb84dd9705879f748a6
alperdrsnn <dursunalper1@gmail.com>
2022-12-31 02:49:18 +0300

    push

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -11,7 +11,7 @@ class ProfileController extends Controller
     public function index(Request $request)
     {
         $data = [];
-        $user = $request->user()->getAttributes();
+        $user = $request->user();
         unset($user['password']);
         unset($user['remember_token']);
         unset($user['school_id']);
