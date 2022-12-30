0032570549e70e9d7c11fea8758911a83567b0b7
alperdrsnn <dursunalper1@gmail.com>
2022-12-31 02:32:46 +0300

    push

--- app/Http/Controllers/Profile/ProfileController.php
+++ app/Http/Controllers/Profile/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Profile;
 
 use App\Http\Controllers\Controller;
+use App\Models\UserMajor;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
@@ -15,6 +16,8 @@ class ProfileController extends Controller
         $user['school'] = $user->school;
         $user['avatar'] = $user->avatar;
         $user['major'] = $user->major->major;
+        $count = UserMajor::where(['school_id' => $user->school_id, 'major_id' => $user->major->major_id])->count();
+        $user['major']['major_user_count'] = $count;
 
         return response(['user' => $user]);
     }
