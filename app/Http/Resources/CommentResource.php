6510efb460eda0949f3c15bca957e62c33bd5567
alperdrsnn <dursunalper1@gmail.com>
2023-01-15 00:02:02 +0300

    push

---
+++ app/Http/Resources/CommentResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CommentResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'content' => $this->body,
+            'avatar' => $this->user->avatar,
+            'parent_id' => $this->parent_id,
+            'replies' => CommentResource::collection($this->replies),
+        ];
+    }
+}
