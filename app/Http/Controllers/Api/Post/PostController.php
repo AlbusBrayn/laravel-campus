9a839e88fdab71bb9ebd0548efa2da36db098535
alperdrsnn <dursunalper1@gmail.com>
2023-01-16 08:20:45 +0300

    push

--- app/Http/Controllers/Api/Post/PostController.php
+++ app/Http/Controllers/Api/Post/PostController.php
@@ -16,7 +16,7 @@ class PostController extends Controller
 
     public function index(Request $request)
     {
-        $posts = Post::where(['published' => 1])->with('comments.replies')->orderBy('created_at', 'desc')->paginate(1);
+        $posts = Post::where(['published' => 1])->with('comments.replies')->orderBy('created_at', 'desc')->paginate(10);
         return PostResource::collection($posts);
     }
 
