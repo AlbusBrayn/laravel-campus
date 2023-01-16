8955153faf4a1335781987522f703f6167522e41
alperdrsnn <dursunalper1@gmail.com>
2023-01-16 10:02:49 +0300

    push

--- app/Http/Controllers/Api/Post/PostController.php
+++ app/Http/Controllers/Api/Post/PostController.php
@@ -17,8 +17,7 @@ class PostController extends Controller
     public function index(Request $request)
     {
         $blockedIds = $request->user()->getBlockedFriendships()->pluck('id')->toArray();
-        dd($blockedIds);
-        $posts = Post::where(['published' => 1])->whereNotIn('user_id', $blockedIds)->with('comments.replies')->orderBy('created_at', 'desc')->paginate(10);
+        $posts = Post::whereNotIn('user_id', $blockedIds)->with('comments.replies')->orderBy('created_at', 'desc')->paginate(10);
         return PostResource::collection($posts);
     }
 
