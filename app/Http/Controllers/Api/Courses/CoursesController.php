66060da85c88b8689986e9d4f68b233005ada2cb
alperdrsnn <dursunalper1@gmail.com>
2023-01-31 02:54:27 +0300

    push

--- app/Http/Controllers/Api/Courses/CoursesController.php
+++ app/Http/Controllers/Api/Courses/CoursesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\Courses;
 
 use App\Http\Controllers\Controller;
+use App\Models\Courses;
 use App\Models\School;
 use Illuminate\Http\Request;
 
@@ -12,9 +13,10 @@ class CoursesController extends Controller
     public function list(Request $request)
     {
         $user = $request->user();
-        $schoolId = School::first();
+        $schoolId = School::first()->id;
         $majorId = $user->major->major_id;
 
-        dd([$schoolId, $majorId]);
+        $courses = Courses::where(['school_id' => $schoolId, 'major_id' => $majorId])->get();
+        dd($courses);
     }
 }
