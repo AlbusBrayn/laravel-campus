735b1ec66f56fcd9d538d85d5b3384e39aaab471
alperdrsnn <dursunalper1@gmail.com>
2023-01-31 04:39:26 +0300

    push

--- app/Http/Resources/TeacherResource.php
+++ app/Http/Resources/TeacherResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Models\UserTeacher;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class TeacherResource extends JsonResource
@@ -14,10 +15,20 @@ class TeacherResource extends JsonResource
      */
     public function toArray($request)
     {
+        $user = $request->user();
+        $selected = false;
+        if ($user) {
+            $userTeacher = UserTeacher::where(['user_id' => $user->id, 'teacher_course_id' => $this->id])->first();
+            if ($userTeacher) {
+                $selected = true;
+            }
+        }
+
         return [
             'id' => $this->teacher->id,
             'name' => $this->teacher->name,
             'is_admin' => (bool)$this->teacher->is_admin,
+            'selected' => $selected
         ];
     }
 }
