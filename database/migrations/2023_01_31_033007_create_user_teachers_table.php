67c422cc504d3ac09f11b476ecfcc4084e560dbf
alperdrsnn <dursunalper1@gmail.com>
2023-01-31 04:10:12 +0300

    push

--- database/migrations/2023_01_31_033007_create_user_teachers_table.php
+++ database/migrations/2023_01_31_033007_create_user_teachers_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('teacher_course_id')->references('id')->on('teachers_courses')->onDelete('cascade');
+            $table->foreign('teacher_course_id')->references('id')->on('teacher_courses')->onDelete('cascade');
         });
     }
 
