03c87debddce966cf1130df488644a8994be839d
alperdrsnn <dursunalper1@gmail.com>
2023-03-11 08:19:21 +0300

    push

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    event(new \App\Events\SendMessageEvent('Hello World', 10, 2));
+    $pusher = new \Pusher\Pusher(config('broadcasting.connections.pusher.key'),
+        config('broadcasting.connections.pusher.secret'),
+        config('broadcasting.connections.pusher.app_id'),
+        config('broadcasting.connections.pusher.options'));
+
+    $pusher->trigger('campus-message', 'test-event', ['message' => 'hello world']);
+    //event(new \App\Events\SendMessageEvent('Hello World', 10, 2));
 });
