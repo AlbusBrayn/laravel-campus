cb736944827d558e7d967c0e82be94e92b9bc3c9
alperdrsnn <dursunalper1@gmail.com>
2023-07-15 00:07:14 +0300

    push

---
+++ app/Http/Controllers/Admin/Crud/UserCommentCrudController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Crud;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserCommentCrudController extends Controller
+{
+    //
+}
