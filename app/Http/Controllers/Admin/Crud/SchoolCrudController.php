9963ac8f5ab556f9614b6995cbec53a1b3f56ede
alperdrsnn <dursunalper1@gmail.com>
2023-07-16 16:02:05 +0300

    push

--- app/Http/Controllers/Admin/Crud/SchoolCrudController.php
+++ app/Http/Controllers/Admin/Crud/SchoolCrudController.php
@@ -19,26 +19,73 @@ class SchoolCrudController extends Controller
 
     public function create()
     {
-        //
+        return view('admin.pages.schools.school-create');
     }
 
     public function createStore(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email_pattern' => 'required|string|max:255',
+            'latitude' => 'required|string|max:255',
+            'longitude' => 'required|string|max:255',
+            'latitude_delta' => 'required|string|max:255',
+            'longitude_delta' => 'required|string|max:255',
+        ]);
+
+        $school = School::create([
+            'name' => $request->name,
+            'email_pattern' => $request->email_pattern,
+            'latitude' => $request->latitude,
+            'longitude' => $request->longitude,
+            'latitude_delta' => $request->latitude_delta,
+            'longitude_delta' => $request->longitude_delta,
+            'is_active' => isset($request->is_active),
+        ]);
+
+        if ($school) {
+            return redirect()->back()->with('success', 'Okul başarıyla oluşturuldu!');
+        } else {
+            return redirect()->back()->with('error', 'Okul oluşturulurken bir hata oluştu!');
+        }
     }
 
     public function update(School $school)
     {
-        //
+        return view('admin.pages.schools.school-update', compact('school'));
     }
 
     public function updateStore(Request $request, School $school)
     {
-        //
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email_pattern' => 'required|string|max:255',
+            'latitude' => 'required|string|max:255',
+            'longitude' => 'required|string|max:255',
+            'latitude_delta' => 'required|string|max:255',
+            'longitude_delta' => 'required|string|max:255',
+        ]);
+
+        $school->name = $request->name;
+        $school->email_pattern = $request->email_pattern;
+        $school->latitude = $request->latitude;
+        $school->longitude = $request->longitude;
+        $school->latitude_delta = $request->latitude_delta;
+        $school->longitude_delta = $request->longitude_delta;
+        $school->is_active = isset($request->is_active);
+
+        if ($school->save()) {
+            return redirect()->back()->with('success', 'Okul başarıyla güncellendi!');
+        } else {
+            return redirect()->back()->with('error', 'Okul güncellenirken bir hata oluştu!');
+        }
     }
 
     public function delete(School $school)
     {
-        //
+        $school->is_active = false;
+        $school->save();
+
+        return redirect()->back()->with('success', 'Okul başarıyla silindi!');
     }
 }
---
+++ resources/views/admin/pages/schools/school-update.blade.php
@@ -0,0 +1,83 @@
+@extends('admin._layouts.dashboard-template')
+@section('content')
+    <main class="workspace">
+        <section class="breadcrumb lg:flex items-start">
+            <div>
+                <h1>Okul Güncelle</h1>
+                <ul>
+                    <li><a class="{{ route('admin.dashboard') }}">Anasayfa</a></li>
+                    <li class="divider la la-arrow-right"></li>
+                    <li><a href="{{ route('admin.schools') }}">Okul Listesi</a></li>
+                    <li class="divider la la-arrow-right"></li>
+                    <li><a>Okul Güncelle</a></li>
+                </ul>
+            </div>
+            <div class="flex flex-wrap gap-2 items-center ltr:ml-auto rtl:mr-auto mt-5 lg:mt-0">
+                <div class="flex gap-x-2">
+                    <button onclick="document.getElementById('schoolForm').submit()" class="btn btn_primary uppercase">Kaydet</button>
+                </div>
+            </div>
+        </section>
+        @if($errors->any())
+            @foreach($errors->all() as $error)
+                <div class="alert alert_danger mb-2">
+                    <strong class="uppercase"><bdi>Error!</bdi></strong>
+                    {{ $error }}
+                    <button class="dismiss la la-times" data-dismiss="alert"></button>
+                </div>
+            @endforeach
+        @endif
+        <form id="schoolForm" action="{{ route('admin.schools.updateStore', ['school' => $school]) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="grid lg:grid-cols-4 gap-5">
+                <div class="lg:col-span-2 xl:col-span-3">
+                    <div class="card p-5">
+                        <div class="mb-5">
+                            <label class="label block mb-2" for="name">Okul Adı</label>
+                            <input type="text" id="name" name="name" class="form-control" value="{{ old('name', $school->name) }}">
+                        </div>
+                        <div class="mb-5">
+                            <label class="label block mb-2" for="email_pattern">Email Paterni</label>
+                            <input type="text" id="email_pattern" name="email_pattern" class="form-control" value="{{ old('email_pattern', $school->email_pattern) }}">
+                        </div>
+                        <div class="mb-5">
+                            <label class="label block mb-2" for="latitude">Enlem</label>
+                            <input type="text" id="latitude" name="latitude" class="form-control" value="{{ old('latitude', $school->latitude) }}">
+                        </div>
+                        <div class="mb-5">
+                            <label class="label block mb-2" for="longitude">Boylam</label>
+                            <input type="text" id="longitude" name="longitude" class="form-control" value="{{ old('longitude', $school->longitude) }}">
+                        </div>
+                        <div class="mb-5">
+                            <label class="label block mb-2" for="latitude_delta">Enlem Delta</label>
+                            <input type="text" id="latitude_delta" name="latitude_delta" class="form-control" value="{{ old('latitude_delta', $school->latitude_delta) }}">
+                        </div>
+                        <div class="mb-5">
+                            <label class="label block mb-2" for="longitude_delta">Boylam Delta</label>
+                            <input type="text" id="longitude_delta" name="longitude_delta" class="form-control" value="{{ old('longitude_delta', $school->longitude_delta) }}">
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-y-5 lg:col-span-2 xl:col-span-1">
+                    <div class="card p-5 flex flex-col gap-y-5">
+                        <h3>Okul Ayarları</h3>
+                        <div class="flex flex-col gap-y-5">
+                            <div class="flex items-center">
+                                <div class="w-3/4">
+                                    <label class="label block">Aktif ?</label>
+                                </div>
+                                <div class="w-1/4 ml-2">
+                                    <label class="switch">
+                                        <input type="checkbox" name="is_active" @if($school->is_active) checked @endif>
+                                        <span></span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </main>
+@endsection
