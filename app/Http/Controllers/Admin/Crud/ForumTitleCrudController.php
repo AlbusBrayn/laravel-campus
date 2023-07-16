9c74e4863605334b6d6e702a86b284fdb9e7956b
alperdrsnn <dursunalper1@gmail.com>
2023-07-16 07:49:15 +0300

    push

--- app/Http/Controllers/Admin/Crud/ForumTitleCrudController.php
+++ app/Http/Controllers/Admin/Crud/ForumTitleCrudController.php
@@ -18,22 +18,43 @@ class ForumTitleCrudController extends Controller
 
     public function create()
     {
-        //
+        return view('admin.pages.forums.forum-title-create');
     }
 
     public function createStore(Request $request)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $data = ["is_active" => isset($request->is_active)];
+        $data["tr"]["title"] = $request->title;
+
+        $postTitle = PostTitle::create($data);
+
+        if ($postTitle) {
+            return redirect()->route('admin.forums.titles')->with('success', 'Forum başlığı başarıyla oluşturuldu!');
+        } else {
+            return redirect()->route('admin.forums.titles')->with('error', 'Forum başlığı oluşturulurken bir hata oluştu!');
+        }
     }
 
-    public function update(PostTitle $post)
+    public function update(PostTitle $postTitle)
     {
-        //
+        return view('admin.pages.forums.forum-title-update', compact('postTitle'));
     }
 
-    public function updateStore(Request $request, PostTitle $post)
+    public function updateStore(Request $request, PostTitle $postTitle)
     {
-        //
+        $request->validate([
+            'title' => 'required|string|max:255',
+        ]);
+
+        $postTitle->is_active = isset($request->is_active);
+        $postTitle->translateOrNew('tr')->title = $request->title;
+        $postTitle->save();
+
+        return redirect()->route('admin.forums.titles')->with('success', 'Forum başlığı başarıyla güncellendi!');
     }
 
     public function delete(PostTitle $postTitle)
---
+++ resources/views/admin/pages/forums/forum-title-update.blade.php
@@ -0,0 +1,63 @@
+@extends('admin._layouts.dashboard-template')
+@section('content')
+    <main class="workspace">
+        <section class="breadcrumb lg:flex items-start">
+            <div>
+                <h1>Forum Başlığı Güncelle</h1>
+                <ul>
+                    <li><a class="{{ route('admin.dashboard') }}">Anasayfa</a></li>
+                    <li class="divider la la-arrow-right"></li>
+                    <li><a href="{{ route('admin.forums.titles') }}">Forum Başlığı Listesi</a></li>
+                    <li class="divider la la-arrow-right"></li>
+                    <li><a>Forum Başlığı Güncelle</a></li>
+                </ul>
+            </div>
+            <div class="flex flex-wrap gap-2 items-center ltr:ml-auto rtl:mr-auto mt-5 lg:mt-0">
+                <div class="flex gap-x-2">
+                    <button onclick="document.getElementById('forumTitleForm').submit()" class="btn btn_primary uppercase">Kaydet</button>
+                </div>
+            </div>
+        </section>
+        @if($errors->any())
+            @foreach($errors->all() as $error)
+                <div class="alert alert_danger mb-2">
+                    <strong class="uppercase"><bdi>Error!</bdi></strong>
+                    {{ $error }}
+                    <button class="dismiss la la-times" data-dismiss="alert"></button>
+                </div>
+            @endforeach
+        @endif
+        <form id="forumTitleForm" action="{{ route('admin.forums.titles.updateStore', ['postTitle' => $postTitle]) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="grid lg:grid-cols-4 gap-5">
+                <div class="lg:col-span-2 xl:col-span-3">
+                    <div class="card p-5">
+                        <div class="mb-5">
+                            <label class="label block mb-2" for="title">Başlık</label>
+                            <input type="text" id="title" name="title" class="form-control" value="{{ old('title', $postTitle->translate('tr')->title) }}">
+                        </div>
+                    </div>
+                </div>
+                <div class="flex flex-col gap-y-5 lg:col-span-2 xl:col-span-1">
+                    <div class="card p-5 flex flex-col gap-y-5">
+                        <h3>Forum Başlığı Ayarları</h3>
+                        <div class="flex flex-col gap-y-5">
+                            <div class="flex items-center">
+                                <div class="w-3/4">
+                                    <label class="label block">Aktif ?</label>
+                                </div>
+                                <div class="w-1/4 ml-2">
+                                    <label class="switch">
+                                        <input type="checkbox" name="is_active" @if($postTitle->is_active) checked @endif>
+                                        <span></span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </main>
+@endsection
